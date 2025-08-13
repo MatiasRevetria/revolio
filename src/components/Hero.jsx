d7d48e073362d6236c5d2f6e61362423799755c3
reve <revetriamatias03@gmail.com>
2025-08-13 02:49:43 -0300

    new branch for mision component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,12 +8,12 @@ const Hero = () => {
         <div className="main-container">
 
             <div id="hero-container">
-                <div>
+                <div id="box1">
                     <div style={{color:'green'}}>AVAILABLE FOR WORK</div>
                     <div><h1 className="text-1">Hi, I'm a Software Engineer</h1></div>
                     <div className="text-container"><p>I'm Matias, a senior Computer Engineering student currently completing the final courses of my degree. For the past five years, I've been dedicated to creating smart solutions to complex problems, applying all the knowledge I've gained both as a student and as a developer. I'm constantly learning and gaining new experiences to further improve my work as a software engineer.</p></div>
                 </div>
-                <div>
+                <div id="box2">
                     <img src={yo} alt="yo"/>
                 </div>
             </div>
--- src/components/Mision.jsx
+++ src/components/Mision.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import '../styles/Mision.css'
 
 const Mision = ()=>{
     return (
         <>
         <div className="mision-container">
-            <article id="mision-article">
-            
-            </article>
+            <section id="mision-article">
+            <h2>My mission is to assist people, startups and enterprises design and develop innovative, reliable, engaging and user-friendly software solutions </h2>
+            </section>
+            <section id="skills">
+            <div id="left-mision">
+
+            </div>
+            <div id="right-mision">
+
+            </div>
+            </section>
         </div>
         </>
     );
