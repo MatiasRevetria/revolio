53fd1b9f822c48dd1cf58e557bf8b6d6fd18fca8
mati <revetriamatias03@gmail.com>
2025-08-19 11:55:28 -0300

    contact backbone created, waiting for another break on my life to continue with the contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../styles/Contact.css'
+
+const Contact = () => {
+
+    return(
+        <>
+        <div id="contact-container">
+            <div id="text">
+
+            </div>
+            <div id="contact-form">
+
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Contact
