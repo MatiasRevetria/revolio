e09aa4b3c94e8edc208db4d97241d944f8e63b42
reve <revetriamatias03@gmail.com>
2025-08-23 14:34:16 -0300

    contact component structure

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,9 +11,9 @@ const Contact = () => {
     const handleSubmit = (event)=>{
         event.preventDefault();
         let validationErrors = {};
-        if (!name) validationErrors.name = "Don't forget your name";
-        if (!email) validationErrors.email = "Don't forget your email";
-        if (!txtArea) validationErrors.txtArea = "What's going on inside that mind?";
+        if (!name.trim()) validationErrors.name = "Don't forget your name";
+        if (!email.trim()) validationErrors.email = "Don't forget your email";
+        if (!txtArea.trim()) validationErrors.txtArea = "What's going on inside that mind?";
 
         if(Object.keys(validationErrors.length)>0){
             setError(validationErrors);
@@ -44,14 +44,30 @@ const Contact = () => {
             <div id="contact-form">
                 <form onSubmit={handleSubmit}>
                     <div className="input">
-                        <input type="text" value={name} onChange={()=>handleChangeName} placeholder="Name"/>
+                        <input type="text" value={name} onChange ={handleChangeName} placeholder="Name" aria-invalid={!!error.name} aria-describedby="name-error"/>
+                        {error.name && 
+                            <small id="name-error" className="error">
+                                {error.name}
+                            </small>
+                        }
                     </div>
                     <div className="input">
-                        <input type="text" value={email} onChange={()=>handleChangeEmail} placeholder="Email"/>
+                        <input type="text" value={email} onChange={handleChangeEmail} placeholder="Email" aria-invalid={!!error.email} aria-describedby="email-error"/>
+                        {error.email &&
+                            <small id="email-error" className="error">
+                                {error.email}
+                            </small>
+                        }
                     </div>
                     <div id="text-area">
-                        <textarea value={txtArea} onChange={()=>handleChangeTxtArea} placeholder="Work description"></textarea>
+                        <textarea value={txtArea} onChange={handleChangeTxtArea} placeholder="Work description"
+                            rows={6} aria-invalid={!!error.txtArea} aria-describedby="message-error">
+                                {error.txtArea && 
+                                    <small id="message-error" className="error">{error.txtArea}</small>
+                                }
+                        </textarea>
                     </div>
+                    <button type="submit" className="btn-send">Send</button>
                 </form>
             </div>
         </div>
