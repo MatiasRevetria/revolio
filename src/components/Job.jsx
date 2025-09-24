4e418fa947846f7952c2e686f16ae3b89e50c84f
mati <revetriamatias03@gmail.com>
2025-09-24 13:48:56 -0300

    Job component created with some changes

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,14 +1,33 @@
 import React from "react";
+import '../styles/Job.css'
 
 const jobs = [
-    {title:'WappMail',description:'WappMail is a massive sending platform for whatsapp and email campaigns. I build it with Typescript, React and Tailwind',time:'1 month'},
-    {title:'WappMail',description:'WappMail is a massive sending platform for whatsapp and email campaigns. I build it with Typescript, React and Tailwind',time:'1 month'},
-    {title:'WappMail',description:'WappMail is a massive sending platform for whatsapp and email campaigns. I build it with Typescript, React and Tailwind',time:'1 month'},
-    {title:'WappMail',description:'WappMail is a massive sending platform for whatsapp and email campaigns. I build it with Typescript, React and Tailwind',time:'1 month'}
+    {title:'WappMail',description:'WappMail is a massive sending platform for whatsapp and email campaigns. I build it with Typescript, React and Tailwind',time:'3 month'},
+    {title:'TalentoTech',description:'',time:'1 month'},
+    {title:'WappMail',description:'This is a website that i have build for a frontend course. Its done with frontend Vanilla',time:'1 month'},
+    {title:'Portfolio',description:'My personal portfolio build it with React and bootstrap',time:'1 month'}
 ]
 
 const Job = ()=> {
 
+    return(
+    <>
+    <div className="main-container">
+        <div className="first-container">
+            <h1>Jobs</h1>
+        </div>
+        <div className="jobs-container">
+        {jobs.map((job,index)=>(
+            <div className="job-card" key={index}>
+                <h1>{job.title}</h1>
+                <p>{job.description}</p>
+                <p>{job.time}</p>
+            </div>
+        ))}
+    </div>
+    </div>
+    </>
+    )
 }
 
 export default Job;
