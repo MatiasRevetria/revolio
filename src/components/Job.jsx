caaeb15826d440f31cfc9c5fc6f9429f40131e9d
mati <revetriamatias03@gmail.com>
2025-09-24 12:24:46 -0300

    job component created

---
+++ src/components/Job.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const jobs = [
+    {title:'WappMail',description:'WappMail is a massive sending platform for whatsapp and email campaigns. I build it with Typescript, React and Tailwind',time:'1 month'},
+    {title:'WappMail',description:'WappMail is a massive sending platform for whatsapp and email campaigns. I build it with Typescript, React and Tailwind',time:'1 month'},
+    {title:'WappMail',description:'WappMail is a massive sending platform for whatsapp and email campaigns. I build it with Typescript, React and Tailwind',time:'1 month'},
+    {title:'WappMail',description:'WappMail is a massive sending platform for whatsapp and email campaigns. I build it with Typescript, React and Tailwind',time:'1 month'}
+]
+
+const Job = ()=> {
+
+}
+
+export default Job;
