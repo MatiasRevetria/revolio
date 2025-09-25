5bf8575b07a83a2bef55305bdfcea53dc30eb995
mati <revetriamatias03@gmail.com>
2025-09-25 12:08:05 -0300

    jobs component ready(1.0.0)

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,28 +1,45 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
 import '../styles/Job.css'
 
-const jobs = [
-    {title:'WappMail',description:'WappMail is a massive sending platform for whatsapp and email campaigns. I build it with Typescript, React and Tailwind',time:'3 month'},
-    {title:'TalentoTech',description:'',time:'1 month'},
-    {title:'WappMail',description:'This is a website that i have build for a frontend course. Its done with frontend Vanilla',time:'1 month'},
-    {title:'Portfolio',description:'My personal portfolio build it with React and bootstrap',time:'1 month'}
-]
 
 const Job = ()=> {
+    const refTitle = useRef();
+    const refJobs = useRef();
+
+    useEffect(()=>{
+        const observer = new IntersectionObserver((entries,obs)=>{
+            entries.forEach((entry)=>{
+                if(entry.isIntersecting){
+                    entry.target.classList.add('show');
+                    obs.unobserve(entry.target);
+                }
+            });
+        },{threshold:0.3});
+        if(refTitle.current) observer.observe(refTitle.current);
+        if(refJobs.current) observer.observe(refJobs.current);
+    },[]);
+
+    const jobs = [
+        {title:'WappMail',description:'WappMail is a massive sending platform for whatsapp and email campaigns. I build it with Typescript, React and Tailwind',time:'3 month',link:'https://wappmail.netlify.app/'},
+        {title:'TalentoTech',description:'This site was build to achive the frontend developer certification. Frontend Vanilla',time:'1 month',link:'https://matiasrevetria.github.io/RevetriaTalentoTech/'},
+        {title:'Portfolio',description:'My personal portfolio build with React and bootstrap',time:'1 month',link:''},            {title:'React final project', description:'This is an ecommerce build with react and bootstrap. The backend is simulated using mockapi to get, create and edit the products',time:'2 month', link:'https://proyecto-final-revetria-2025-react.netlify.app/'}
+        ]
 
     return(
     <>
     <div className="main-container">
-        <div className="first-container">
+        <div className="first-container appear-y" ref={refTitle}>
             <h1>Jobs</h1>
         </div>
-        <div className="jobs-container">
+        <div className="jobs-container appear-x" ref={refJobs}>
         {jobs.map((job,index)=>(
-            <div className="job-card" key={index}>
+            <a href={job.link}>
+                <button className="job-card" key={index}>
                 <h1>{job.title}</h1>
                 <p>{job.description}</p>
                 <p>{job.time}</p>
-            </div>
+            </button>
+            </a>
         ))}
     </div>
     </div>
